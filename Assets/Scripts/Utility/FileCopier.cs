10b3127b2a5b6cbafdac15e35bf351d44a5c3b3f
mercedes sandu <mxrcedes@gmail.com>
2024-02-06 19:03:13 -0600

    file copier does diffs now

--- Assets/Scripts/Utility/FileCopier.cs
+++ Assets/Scripts/Utility/FileCopier.cs
@@ -16,16 +16,21 @@ namespace Utility
         /// </summary>
         public static void CheckForCopies()
         {
-            var allImaginariumFiles = Resources.LoadAll<TextAsset>("Imaginarium");
-            var allInflectionsFiles = Resources.LoadAll<TextAsset>("Inflections");
+            TextAsset[] allImaginariumFiles = Resources.LoadAll<TextAsset>("Imaginarium");
+            TextAsset[] allInflectionsFiles = Resources.LoadAll<TextAsset>("Inflections");
             
             foreach (var file in allImaginariumFiles)
             {
-                var fileName = $"{file.name}{(file.text.Contains(GenFileIndicator) ? ".gen" : ".txt")}";
-                Debug.Log($"checking file {fileName}");
-                if (!File.Exists(Path.Combine(PersistentDataPath, fileName)))
+                string fileName = $"{file.name}{(file.text.Contains(GenFileIndicator) ? ".gen" : ".txt")}";
+                Debug.Log($"Checking for copy of {fileName}.");
+                string filePath = Path.Combine(PersistentDataPath, fileName);
+                if (!File.Exists(filePath))
                 {
                     CopyFile(file, fileName);
+                }
+                else
+                {
+                    CheckFileDiff(file, File.ReadAllText(filePath), fileName);
                 }
             }
             
@@ -33,20 +38,42 @@ namespace Utility
             
             foreach (var file in allInflectionsFiles)
             {
-                var fileName = $"{file.name}{(file.text.Contains(CsvFileIndicator) ? ".csv" : ".txt")}";
-                Debug.Log($"checking file {fileName}");
-                if (!File.Exists(Path.Combine(PersistentDataPath, InflectionsFolder, fileName)))
+                string fileName = $"{file.name}{(file.text.Contains(CsvFileIndicator) ? ".csv" : ".txt")}";
+                Debug.Log($"Checking for copy of {fileName}.");
+                string filePath = Path.Combine(PersistentDataPath, InflectionsFolder, fileName);
+                if (!File.Exists(filePath))
                 {
                     CopyFile(file, Path.Combine(InflectionsFolder, fileName));
+                }
+                else
+                {
+                    CheckFileDiff(file, File.ReadAllText(filePath), Path.Combine(InflectionsFolder, fileName));
                 }
             }
         }
 
-        // todo write something that checks file dif
-
+        /// <summary>
+        /// Checks if the newer file is different from the older file. If it is, the newer file is copied to the
+        /// player's local files, replacing the older file since it is copied under the same name.
+        /// </summary>
+        /// <param name="newerFile">The newer file as a TextAsset.</param>
+        /// <param name="olderFileContents">The older file's string contents.</param>
+        /// <param name="fileName">The file's name that is being compared.</param>
+        private static void CheckFileDiff(TextAsset newerFile, string olderFileContents, string fileName)
+        {
+            // todo: more efficient/better way to do this than Equals()?
+            if (newerFile.text.Equals(olderFileContents)) return;
+            Debug.Log($"Found difference for {fileName}.");
+            CopyFile(newerFile, fileName);
+        }
+        
+        /// <summary>
+        /// Creates a folder for inflections in the player's local data if it doesn't already exist.
+        /// </summary>
         private static void MakeInflectionDirectory()
         {
             if (Directory.Exists(Path.Combine(PersistentDataPath, InflectionsFolder))) return;
+            Debug.Log($"Creating Inflections folder.");
             Directory.CreateDirectory(Path.Combine(PersistentDataPath, InflectionsFolder));
         }
         
@@ -59,6 +86,7 @@ namespace Utility
         {
             string fileText = fileName.Contains(".csv") ? file.text.Replace(CsvFileIndicator, "") : file.text;
             File.WriteAllText(Path.Combine(PersistentDataPath, fileName), fileText);
+            Debug.Log($"Copied {fileName} to {PersistentDataPath}.");
         }
     }
 }
